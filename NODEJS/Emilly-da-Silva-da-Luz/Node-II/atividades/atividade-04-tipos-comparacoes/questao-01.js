0f317e1fdd3ad6ac95cb04545effc856c76885bb
Emilly12321 <emillysilvaluz2839@gmail.com>
2026-09-09 08:57:51 -0300

    add: atividades

---
+++ NODEJS/Emilly-da-Silva-da-Luz/Node-II/atividades/atividade-04-tipos-comparacoes/questao-01.js
@@ -0,0 +1,5 @@
+console.log(0 == "0");      // true
+console.log(0 === "0");     // false
+console.log(false == 0);     // true
+console.log(false === 0);    // false
+console.log(1 == true);      // true
---
+++ NODEJS/Emilly-da-Silva-da-Luz/Node-II/atividades/atividade-04-tipos-comparacoes/questao-03.js
@@ -0,0 +1,3 @@
+const valor = Number("abc");
+console.log(valor);               // NaN
+console.log(Number.isNaN(valor)); // true
