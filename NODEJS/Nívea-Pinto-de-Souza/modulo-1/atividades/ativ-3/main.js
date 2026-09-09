86e1e85f64a06a85b227728ebe366f399334474a
niveabeh <niveasouza86@gmail.com>
2026-09-09 00:05:11 -0300

    atividade 3

---
+++ NODEJS/Nívea-Pinto-de-Souza/modulo-1/atividades/ativ-3/main.js
@@ -0,0 +1,7 @@
+const nomeServico = "Consulta";
+let quantidadeVagas = 10;
+
+quantidadeVagas = 9;
+
+/*Usamos const em nomeServico porque esse valor não precisa ser reatribuído.
+Já quantidadeVagas usa let porque seu valor pode mudar durante a execução.*/ 
