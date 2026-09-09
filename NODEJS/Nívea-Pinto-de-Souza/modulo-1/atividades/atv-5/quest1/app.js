c90b9a970f7fa0572ff6f39f43d1dfd0a911d719
niveabeh <niveasouza86@gmail.com>
2026-09-09 00:18:19 -0300

    add atv 5 q 1

---
+++ NODEJS/Nívea-Pinto-de-Souza/modulo-1/atividades/atv-5/quest1/app.js
@@ -0,0 +1,10 @@
+const name = "Consulta";
+const price = 150;
+const durationMinutes = 45;
+
+const message = `Serviço: ${name} | Preço: R$ ${price} | Duração: ${durationMinutes} minutos`;
+
+console.log(message);
+
+/*Template literals permitem inserir variáveis diretamente dentro de uma string usando ${}, 
+deixando o código mais legível e evitando várias concatenações.*/ 
