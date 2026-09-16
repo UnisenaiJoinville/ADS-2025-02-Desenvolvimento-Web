b797da6642dba971b08ecf63b73140d4295d5441
lucas-travassos <lucasstravassos19@gmail.com>
2026-09-16 07:38:19 -0300

    feat: implementa CRUD de produtos com filtros e validacoes (Etapa 12)

--- PROJETOS/Lucas-Eduardo-Travassos-Machado/EstoqueFacil/src/routes/index.js
+++ PROJETOS/Lucas-Eduardo-Travassos-Machado/EstoqueFacil/src/routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { categoryRoutes } from "../modules/categories/category-routes.js";
+import { productRoutes } from "../modules/products/product-routes.js";
 
 export const routes = Router();
 
@@ -9,3 +10,4 @@ routes.get("/health", (request, response) => {
 });
 
 routes.use("/categories", categoryRoutes);
+routes.use("/products", productRoutes);
