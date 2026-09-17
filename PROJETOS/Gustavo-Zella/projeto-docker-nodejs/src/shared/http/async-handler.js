91b1d041b22d63008e64409c767a85355ade6503
Gustavo Zella <gustavozella21@gmail.com>
2026-09-16 21:40:15 -0300

    Zella estoquefacil (#47)
    * Envio da atividade - Gustavo Zella
    
    * Enviando Projeto Estoque Fácil

---
+++ PROJETOS/Gustavo-Zella/projeto-docker-nodejs/src/shared/http/async-handler.js
@@ -0,0 +1,7 @@
+// O Express 4 nao captura erros de funcoes async automaticamente.
+// Este wrapper encaminha qualquer rejeicao para o middleware de erro.
+export function asyncHandler(handler) {
+  return (request, response, next) => {
+    Promise.resolve(handler(request, response, next)).catch(next);
+  };
+}
