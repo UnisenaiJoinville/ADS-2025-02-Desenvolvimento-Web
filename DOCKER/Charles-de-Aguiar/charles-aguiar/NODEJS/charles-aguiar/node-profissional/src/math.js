b345ec682187563726b5eddcd36fd5563468eec3
charlesaguiar93 <charlesaguiar93@gmail,com>
2026-09-17 17:02:39 -0300

    feat:atualizar arquivos e pastas da raiz do projeto

--- DOCKER/Charles-de-Aguiar/charles-aguiar/NODEJS/charles-aguiar/node-profissional/src/math.js
+++
@@ -1,2 +0,0 @@
-export function somar(a, b) { return a + b; }
-export function subtrair(a, b) { return a - b; }
--- DOCKER/Charles-de-Aguiar/charles-aguiar/NODEJS/charles-aguiar/node-profissional/src/service.js
+++
@@ -1,5 +0,0 @@
-import { validarNomeServico } from "./service-validator.js";
-export function criarServico(name, price) {
-  validarNomeServico(name);
-  return { name, price, createdAt: new Date() };
-}
